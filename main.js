05a84f69a34cea739069f0109736ec778e43cdc5
Code Programer <93539221+code-programer@users.noreply.github.com>
2023-02-02 09:24:13 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ var data = {}
 function onload(c){
   var e = c;
   for(b in e){
-    data[b[e]] = new DB(b[e], "$2b$10$GQBXgX0qtSt9SMrLTUYkle8r3ine3QESBb8jK69jddclQRRmMufvS");
-    data[b[e]].get()
+    data[b[e].id] = new DB(b[e].id, "$2b$10$GQBXgX0qtSt9SMrLTUYkle8r3ine3QESBb8jK69jddclQRRmMufvS");
+    data[b[e].id].get()
   }
 }
 window.onload = function(){
