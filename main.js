24647ca7cf3afd166ac5befe9941e06ae76743d5
Code Programer <93539221+code-programer@users.noreply.github.com>
2023-02-02 09:18:21 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-var db = new DB("63db701cace6f33a22d38aaf","$2b$10$GQBXgX0qtSt9SMrLTUYkle8r3ine3QESBb8jK69jddclQRRmMufvS");
+var data = {}
+function onload(e){
+  for(b in e){
+    data[b[e]] = new DB(b[e], "$2b$10$GQBXgX0qtSt9SMrLTUYkle8r3ine3QESBb8jK69jddclQRRmMufvS");
+    data[b[e]].get()
+  }
+}
+window.onload = function(){
+  var db = new DB("63db701cace6f33a22d38aaf","$2b$10$GQBXgX0qtSt9SMrLTUYkle8r3ine3QESBb8jK69jddclQRRmMufvS");
+  db.get("onload")
+}
