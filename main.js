32b6aff40c8b3ac7f928123c0720e764bea76c41
Code Programer <93539221+code-programer@users.noreply.github.com>
2023-02-02 20:02:19 +0100

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ function doload(e){
     data.push(new DB(e[b].id, "$2b$10$GQBXgX0qtSt9SMrLTUYkle8r3ine3QESBb8jK69jddclQRRmMufvS"))
     data[b].get()
   }
+  loadtotaldata()
 }
 function loadtotaldata(){
   for(base in data){
@@ -19,3 +20,12 @@ function loadtotaldata(){
 }
 var db = new DB("63db7034ace6f33a22d38abd","$2b$10$GQBXgX0qtSt9SMrLTUYkle8r3ine3QESBb8jK69jddclQRRmMufvS");
 db.get("doload")
+function search(e){
+  var returner = []
+  for(el in totaldata){
+    if(totaldata[el].text.includes(e)){
+      returner.push(totaldata[el].anserwers)
+    }
+  }
+  return returner;
+}
