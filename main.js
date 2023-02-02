c531fb7e01a69fc99a050a0f50a26f1f0d73db52
Code Programer <93539221+code-programer@users.noreply.github.com>
2023-02-02 19:56:52 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 //requires db.js
-var data = {}
-function onload(c){
-  var e = c;
+var data = []
+var totaldata = []
+function doload(e){
+  console.log(e)
   for(b in e){
-    data[e[b].id] = new DB(e[b].id, "$2b$10$GQBXgX0qtSt9SMrLTUYkle8r3ine3QESBb8jK69jddclQRRmMufvS");
-    data[e[b].id].get()
+    console.log(e[b])
+    data.push(new DB(e[b].id, "$2b$10$GQBXgX0qtSt9SMrLTUYkle8r3ine3QESBb8jK69jddclQRRmMufvS"))
+    data[b].get()
   }
 }
-window.onload = function(){
-  var db = new DB("63db7034ace6f33a22d38abd","$2b$10$GQBXgX0qtSt9SMrLTUYkle8r3ine3QESBb8jK69jddclQRRmMufvS");
-  db.get("onload")
+function loadtotaldata(){
+  for(base in data){
+    var database = data[base].data;
+    for(el in database){
+      totaldata.push(database[el])
+    }
+  }
 }
+var db = new DB("63db7034ace6f33a22d38abd","$2b$10$GQBXgX0qtSt9SMrLTUYkle8r3ine3QESBb8jK69jddclQRRmMufvS");
+db.get("doload")
