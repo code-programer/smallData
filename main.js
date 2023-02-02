e684a5130c90db592c80d7384d38ff1000844b1e
Code Programer <93539221+code-programer@users.noreply.github.com>
2023-02-02 09:21:44 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 //requires db.js
 var data = {}
-function onload(e){
+function onload(c){
+  var e = c;
   for(b in e){
     data[b[e]] = new DB(b[e], "$2b$10$GQBXgX0qtSt9SMrLTUYkle8r3ine3QESBb8jK69jddclQRRmMufvS");
     data[b[e]].get()
   }
 }
 window.onload = function(){
-  var db = new DB("63db701cace6f33a22d38aaf","$2b$10$GQBXgX0qtSt9SMrLTUYkle8r3ine3QESBb8jK69jddclQRRmMufvS");
+  var db = new DB("63db7034ace6f33a22d38abd","$2b$10$GQBXgX0qtSt9SMrLTUYkle8r3ine3QESBb8jK69jddclQRRmMufvS");
   db.get("onload")
 }
